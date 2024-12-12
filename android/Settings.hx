98b3a84d7f0040224c5af06deee0eb636107762d
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-12-12 08:09:45 +0200

    Update Settings.hx

--- android/Settings.hx
+++ android/Settings.hx
@@ -19,9 +19,11 @@ class Settings
 	 *                it will be prefixed with that string automatically.
 	 * @param requestCode The request code to be passed to the JNI method.
 	 */
-	public static inline function requestSetting(setting:String, requestCode:Int = 1):Void
+	public static function requestSetting(setting:String, requestCode:Int = 1):Void
 	{
-		JNICache.createStaticMethod('org/haxe/extension/Tools', 'requestSetting',
-			'(Ljava/lang/String;I)V')(!setting.startsWith('android.settings.') ? 'android.settings.$setting' : setting, requestCode);
+		final requestSettingJNI:Null<Dynamic> = JNICache.createStaticMethod('org/haxe/extension/Tools', 'requestSetting', '(Ljava/lang/String;I)V');
+
+		if (requestSettingJNI != null)
+			requestSettingJNI(!setting.startsWith('android.settings.') ? 'android.settings.$setting' : setting, requestCode);
 	}
 }
